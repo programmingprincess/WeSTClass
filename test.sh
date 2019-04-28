8d631846f2a704644635bafcc2727a105dac2e8b
programmingprincess <jiaqiwu@asu.edu>
2019-04-28 16:14:54 -0500

    fix spacing

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 
 dataset=github_readmes
+# sup_source=keywords
+# model=cnn
+
+#dataset=yelp
 sup_source=keywords
 model=cnn
-
 export CUDA_VISIBLE_DEVICES=0
 
 python3 main.py --dataset ${dataset} --sup_source ${sup_source} --model ${model} --with_evaluation True
